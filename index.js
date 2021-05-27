518ca0ea334993c302ea5f114a6daf8f14e6bc00
irfan marzuki <irfnmzk@gmail.com>
2021-05-27 23:15:06 +0700

    add sharp png

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
 const express = require("express");
+const sharp = require("sharp");
+
 const { renderSvg, generateFaces } = require("./lib/face");
 const htmlSafe = require("./lib/html-safe");
 
@@ -25,7 +27,27 @@ app.get("/faces/:id/image.svg", async (req, res) => {
 
   const text = htmlSafe(generateFaces(id));
 
-  const image = renderSvg(text);
+  const svg = renderSvg(text);
+
+  res.type("svg").send(Buffer.from(svg));
+});
+
+app.get("/faces/:id/image.png", async (req, res) => {
+  const { id } = req.params;
+
+  const text = htmlSafe(generateFaces(id));
+
+  const svg = renderSvg(text);
+
+  try {
+    const png = await sharp(Buffer.from(svg), { density: 300 })
+      .png()
+      .toBuffer();
+
+    return res.type("png").send(png);
+  } catch (err) {
+    console.log(err);
+  }
 
   res.type("svg").send(Buffer.from(image));
 });
